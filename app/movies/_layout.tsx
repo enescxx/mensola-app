a9e9339d6cf0e974e18f35a477a1922d6631ef5a
Enes Can <github@enesc.me>
2026-08-16 21:11:30 +0300

    fix(mobile): update movie interactions route path to plural /movies

--- app/movies/_layout.tsx
+++ app/movies/_layout.tsx
@@ -4,6 +4,7 @@ export default function AuthLayout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="[movieId]/index" />
+            <Stack.Screen name="[movieId]/interactions" />
         </Stack>
     );
 }
