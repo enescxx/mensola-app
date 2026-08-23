00282d5a715e040c3518adfb8875dcb8501d81d3
Enes Can <enescx54@gmail.com>
2026-08-23 21:11:34 +0300

    feat: add findOrFetch methods for movies and tracks, and refactor services

--- services/movie.service.ts
+++ services/movie.service.ts
@@ -1,4 +1,4 @@
-import { MovieId, MovieListId } from "@/types/common.types";
+import { MovieId, MovieListId, TmdbId } from "@/types/common.types";
 import { client } from "../api/client";
 import {
     InteractionsRequest,
@@ -20,58 +20,62 @@ import {
 import { ApiResponse } from "@/types/api";
 const MovieService = {
     getMovie: async (movieId: MovieId): Promise<MovieDetailsResponse> => {
-        return client.get<MovieDetailsResponse>(`/v1/movies/${movieId}`, { auth: true });
+        return await client.get<MovieDetailsResponse>(`/v1/movies/${movieId}`, { auth: true });
     },
 
     markAsWatched: async (movieId: MovieId): Promise<MarkAsWatchedResponse> => {
-        return client.post<MarkAsWatchedResponse>(`/v1/movies/${movieId}/watched`, {}, { auth: true });
+        return await client.post<MarkAsWatchedResponse>(`/v1/movies/${movieId}/watched`, {}, { auth: true });
     },
 
     unmarkAsWatched: async (movieId: MovieId): Promise<ApiResponse> => {
-        return client.delete<ApiResponse>(`/v1/movies/${movieId}/watched`, { auth: true });
+        return await client.delete<ApiResponse>(`/v1/movies/${movieId}/watched`, { auth: true });
     },
 
     likeMovie: async (movieId: MovieId): Promise<MovieLikeActionsResponse> => {
-        return client.post<MovieLikeActionsResponse>(`/v1/movies/${movieId}/like`, {}, { auth: true });
+        return await client.post<MovieLikeActionsResponse>(`/v1/movies/${movieId}/like`, {}, { auth: true });
     },
 
     unlikeMovie: async (movieId: MovieId): Promise<MovieLikeActionsResponse> => {
-        return client.delete<MovieLikeActionsResponse>(`/v1/movies/${movieId}/like`, { auth: true });
+        return await client.delete<MovieLikeActionsResponse>(`/v1/movies/${movieId}/like`, { auth: true });
     },
 
     addToWatchlist: async (movieId: MovieId): Promise<AddToListResponse> => {
-        return client.post<AddToListResponse>(`/v1/movies/${movieId}/watchlist`, {}, { auth: true });
+        return await client.post<AddToListResponse>(`/v1/movies/${movieId}/watchlist`, {}, { auth: true });
     },
 
     removeFromWatchlist: async (movieId: MovieId): Promise<ApiResponse> => {
-        return client.delete<ApiResponse>(`/v1/movies/${movieId}/watchlist`, { auth: true });
+        return await client.delete<ApiResponse>(`/v1/movies/${movieId}/watchlist`, { auth: true });
     },
 
     addMovieToList: async (listId: MovieListId, movieId: MovieId): Promise<AddToListResponse> => {
-        return client.post<AddToListResponse>(`/v1/movies/lists/${listId}/items/${movieId}`, {}, { auth: true });
+        return await client.post<AddToListResponse>(`/v1/movies/lists/${listId}/items/${movieId}`, {}, { auth: true });
     },
 
     removeMovieFromList: async (listId: MovieListId, movieId: MovieId): Promise<ApiResponse> => {
-        return client.delete<ApiResponse>(`/v1/movies/lists/${listId}/items/${movieId}`, { auth: true });
+        return await client.delete<ApiResponse>(`/v1/movies/lists/${listId}/items/${movieId}`, { auth: true });
     },
 
     getUserLists: async (data: GetListsRequest): Promise<GetListsResponse> => {
         const { movieId, page = 1, limit = 20 } = data;
-        return client.get<GetListsResponse>(`/v1/movies/lists`, { auth: true, params: { movieId, page, limit } });
+        return await client.get<GetListsResponse>(`/v1/movies/lists`, { auth: true, params: { movieId, page, limit } });
     },
 
     createOrUpdateInteraction: async (data: UpsertInteractionRequest): Promise<UpsertInteractionResponse> => {
-        return client.post<UpsertInteractionResponse>(`/v1/movies/${data.targetId}/interaction`, data.interaction, {
-            auth: true,
-        });
+        return await client.post<UpsertInteractionResponse>(
+            `/v1/movies/${data.targetId}/interaction`,
+            data.interaction,
+            {
+                auth: true,
+            },
+        );
     },
 
     getMovieListDetails: async (listId: MovieListId): Promise<MovieListDetailsResponse> => {
-        return client.get<MovieListDetailsResponse>(`/v1/movies/lists/${listId}`, { auth: true });
+        return await client.get<MovieListDetailsResponse>(`/v1/movies/lists/${listId}`, { auth: true });
     },
 
     getMovieListItems: async (listId: string, page = 1, limit = 18): Promise<MovieListItemsResponse> => {
-        return client.get<MovieListItemsResponse>(`/v1/movies/lists/${listId}/items`, {
+        return await client.get<MovieListItemsResponse>(`/v1/movies/lists/${listId}/items`, {
             auth: true,
             params: { page, limit },
         });
@@ -79,14 +83,14 @@ const MovieService = {
 
     getMovieListInteractions: async (data: InteractionsRequest): Promise<InteractionsResponse> => {
         const { targetId, page = 1, limit = 15 } = data;
-        return client.get<InteractionsResponse>(`/v1/movies/lists/${targetId}/interactions`, {
+        return await client.get<InteractionsResponse>(`/v1/movies/lists/${targetId}/interactions`, {
             auth: true,
             params: { page, limit },
         });
     },
 
     createOrUpdateListInteraction: async (data: UpsertInteractionRequest): Promise<UpsertInteractionResponse> => {
-        return client.post<UpsertInteractionResponse>(
+        return await client.post<UpsertInteractionResponse>(
             `/v1/movies/lists/${data.targetId}/interaction`,
             data.interaction,
             { auth: true },
@@ -94,11 +98,15 @@ const MovieService = {
     },
 
     likeMovieList: async (listId: MovieListId): Promise<MovieListLikeActionsResponse> => {
-        return client.post<MovieListLikeActionsResponse>(`/v1/movies/lists/${listId}/like`, {}, { auth: true });
+        return await client.post<MovieListLikeActionsResponse>(`/v1/movies/lists/${listId}/like`, {}, { auth: true });
     },
 
     unlikeMovieList: async (listId: MovieListId): Promise<MovieListLikeActionsResponse> => {
-        return client.delete<MovieListLikeActionsResponse>(`/v1/movies/lists/${listId}/like`, { auth: true });
+        return await client.delete<MovieListLikeActionsResponse>(`/v1/movies/lists/${listId}/like`, { auth: true });
+    },
+
+    findOrFetchMovie: async (tmdbId: TmdbId): Promise<MovieDetailsResponse> => {
+        return await client.get<MovieDetailsResponse>(`/v1/movies/by-tmdb/${tmdbId}`, { auth: true });
     },
 };
 
--- services/track.service.ts
+++ services/track.service.ts
@@ -1,25 +1,31 @@
-import { TrackId } from "@/types/common.types";
+import { SpotifyId, TrackId } from "@/types/common.types";
 import { client } from "../api/client";
 import { UpsertInteractionRequest, UpsertInteractionResponse } from "@/types/interaction.types";
 import { TrackDetailsResponse, TrackLikeActionsResponse } from "@/types/track.types";
 
 const TrackService = {
     getTrackDetails: async (trackId: TrackId): Promise<TrackDetailsResponse> => {
-        return client.get<TrackDetailsResponse>(`/v1/tracks/${trackId}`, { auth: true });
+        return await client.get<TrackDetailsResponse>(`/v1/tracks/${trackId}`, { auth: true });
     },
 
     likeTrack: async (trackId: TrackId): Promise<TrackLikeActionsResponse> => {
-        return client.post<TrackLikeActionsResponse>(`/v1/tracks/${trackId}/like`, {}, { auth: true });
+        return await client.post<TrackLikeActionsResponse>(`/v1/tracks/${trackId}/like`, {}, { auth: true });
     },
 
     unlikeTrack: async (trackId: TrackId): Promise<TrackLikeActionsResponse> => {
-        return client.delete<TrackLikeActionsResponse>(`/v1/tracks/${trackId}/like`, { auth: true });
+        return await client.delete<TrackLikeActionsResponse>(`/v1/tracks/${trackId}/like`, { auth: true });
     },
 
     createOrUpdateInteraction: async (data: UpsertInteractionRequest): Promise<UpsertInteractionResponse> => {
-        return client.post<UpsertInteractionResponse>(`/v1/tracks/${data.targetId}/interactions`, data.interaction, {
-            auth: true,
-        });
+        return await client.post<UpsertInteractionResponse>(
+            `/v1/tracks/${data.targetId}/interactions`,
+            data.interaction,
+            { auth: true },
+        );
+    },
+
+    findOrFetchSpotifyTrack: async (spotifyId: SpotifyId): Promise<TrackDetailsResponse> => {
+        return await client.get<TrackDetailsResponse>(`/v1/tracks/by-spotify/${spotifyId}`, { auth: true });
     },
 };
 
